4ebd86a670f4d38ae21f33513ea1222c02e6795f
nanowl <hanjy20129@gmail.com>
2023-04-26 20:07:16 +0900

    Initial Commit

---
+++ src/main/java/com/kh/ecoFriend/EcoFriendApplication.java
@@ -0,0 +1,13 @@
+package com.kh.ecoFriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/ecoFriend/EcoFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.ecoFriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
